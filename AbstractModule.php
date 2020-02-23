7b57fddef9f5c907d569ee182c955c02122fdd2a
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-02-24 00:00:00 +0100

    Cleaned code.

--- AbstractModule.php
+++ AbstractModule.php
@@ -641,9 +641,7 @@ abstract class AbstractModule extends \Omeka\Module\AbstractModule
      */
     public function stringToList($string)
     {
-        return array_filter(array_map('trim', explode("\n", $this->fixEndOfLine($string))), function ($v) {
-            return (bool) strlen($v);
-        });
+        return array_filter(array_map('trim', explode("\n", $this->fixEndOfLine($string))), 'strlen');
     }
 
     /**
