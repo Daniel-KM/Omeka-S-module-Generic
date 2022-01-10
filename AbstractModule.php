b5473c805088986f42e500cbbfa6f018203ee5ab
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-01-10 00:00:00 +0000

    Released version 3.3.32.

--- AbstractModule.php
+++ AbstractModule.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 /*
- * Copyright Daniel Berthereau, 2018-2021
+ * Copyright Daniel Berthereau, 2018-2022
  *
  * This software is governed by the CeCILL license under French law and abiding
  * by the rules of distribution of free software.  You can use, modify and/ or
