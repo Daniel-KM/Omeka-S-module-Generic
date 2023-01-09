ba401743ce3fbc7e7ba03a9bd58c6b372e2c1025
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-09 00:00:00 +0000

    Revert "Removed method checkDependency()." for compatibility with Omeka S v3 and v4.
    This reverts commit 69d42898514dea37ba6fcb269897e70f8531a4d2.

--- AbstractModule.php
+++ AbstractModule.php
@@ -75,6 +75,13 @@ abstract class AbstractModule extends \Omeka\Module\AbstractModule
         $this->setServiceLocator($services);
         $translator = $services->get('MvcTranslator');
         $this->preInstall();
+        if (!$this->checkDependency()) {
+            $message = new Message(
+                $translator->translate('This module requires the module "%s".'), // @translate
+                $this->dependency
+            );
+            throw new ModuleCannotInstallException((string) $message);
+        }
         if (!$this->checkDependencies()) {
             if (count($this->dependencies) === 1) {
                 $message = new Message(
@@ -793,6 +800,19 @@ abstract class AbstractModule extends \Omeka\Module\AbstractModule
             $data[$name] = $val;
         }
         return $data;
+    }
+
+    /**
+     * Check if the module has a dependency.
+     *
+     * This method is distinct of checkDependencies() for performance purpose.
+     *
+     * @return bool
+     */
+    protected function checkDependency(): bool
+    {
+        return empty($this->dependency)
+            || $this->isModuleActive($this->dependency);
     }
 
     /**
